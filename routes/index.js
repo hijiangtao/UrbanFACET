604edae1c50b427e0faaa3b623cf4e34551f6107
Joe Jiang <hijiangtao@gmail.com>
2016-12-18 15:00:38 +0800

    UI adjust in lab and index pages

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'LMVC', name: 'index' });
+  res.render('index', { title: 'Home', name: 'index' });
 });
 /* GET stat page. */
 router.get('/lab', function(req, res, next) {
-  res.render('lab', { title: 'LMVC', name: 'lab' });
+  res.render('lab', { title: 'Lab Stats', name: 'lab' });
 });
 
 module.exports = router;
