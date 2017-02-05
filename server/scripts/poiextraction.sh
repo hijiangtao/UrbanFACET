f7f892b0bcb229e18ffa4bdfea73a91d4a683ea2
Joe Jiang <hijiangtao@gmail.com>
2017-02-05 19:11:12 +0800

    fix: data paths problem

--- server/scripts/poiextraction.sh
+++ server/scripts/poiextraction.sh
@@ -1,9 +1,9 @@
 # 
 
 cd /home/taojiang/git/living-modes-visual-comparison/server/scripts
-python ./POIExtraction.py -c beijing -d /home/taojiang/datasets/pois
-# python ./POIExtraction.py -c tianjin -d /home/taojiang/datasets/pois
-# python ./POIExtraction.py -c zhangjiakou -d /home/taojiang/datasets/pois
-# python ./POIExtraction.py -c tangshan -d /home/taojiang/datasets/pois
+python ./POIExtraction.py -c beijing -d /home/taojiang/datasets/JingJinJi/pois
+# python ./POIExtraction.py -c tianjin -d /home/taojiang/datasets/JingJinJi/pois
+# python ./POIExtraction.py -c zhangjiakou -d /home/taojiang/datasets/JingJinJi/pois
+# python ./POIExtraction.py -c tangshan -d /home/taojiang/datasets/JingJinJi/pois
 
-# mongoimport --db tonormal -c pois_beijing --file '/home/taojiang/datasets/pois/beijing/beijing.aPOI.geojson' --jsonArray -batchSize 1
+# mongoimport --db tonormal -c pois_beijing --file '/home/taojiang/datasets/JingJinJi/pois/beijing/beijing.aPOI.geojson' --jsonArray -batchSize 1
