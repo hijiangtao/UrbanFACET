a23dab33ab485b61eb85d947b7de3e12cda35251
Joe Jiang <hijiangtao@gmail.com>
2017-02-06 19:07:19 +0800

    update: gir repo directory in td10

--- server/scripts/augmentrawdata.sh
+++ server/scripts/augmentrawdata.sh
@@ -1,4 +1,4 @@
 #
 
-cd /home/taojiang/git/living-modes-visual-comparison/server/scripts
+cd /home/tao.jiang/git/living-modes-visual-comparison/server/scripts
 python ./augmentRawData.py -c zhangjiakou -d /home/tao.jiang/datasets/JingJinJi/records -n 20
