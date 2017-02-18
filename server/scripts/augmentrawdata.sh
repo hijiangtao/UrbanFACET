f6b474b7e531a653d6245e655e7ac47e9b51ac56
Joe Jiang <hijiangtao@gmail.com>
2017-02-18 23:16:22 +0800

    update

--- server/scripts/augmentrawdata.sh
+++ server/scripts/augmentrawdata.sh
@@ -3,7 +3,7 @@
 cd /home/tao.jiang/git/living-modes-visual-comparison/server/scripts
 python ./augmentRawData.py -c tangshan -d /home/tao.jiang/datasets/JingJinJi/records -n 723
 python ./augmentRawData.py -c zhangjiakou -d /home/tao.jiang/datasets/JingJinJi/records -n 264
-# python ./augmentRawData.py -c tianjin -d /home/tao.jiang/datasets/JingJinJi/records -n 999
+python ./augmentRawData.py -c tianjin -d /home/tao.jiang/datasets/JingJinJi/records -n 999
 # zhangjiakou 264
 # tangshan 723
 # tianjin 999
