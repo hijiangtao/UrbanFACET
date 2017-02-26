f99249dac2a1be06b3007f9446607d5e8cf79581
Joe Jiang <hijiangtao@gmail.com>
2017-02-26 14:16:22 +0800

    add: timeperiod version entropy calculation script (people entropy)

---
+++ server/scripts/tpenpmatrixmodule.sh
@@ -0,0 +1,10 @@
+
+cd /home/tao.jiang/git/living-modes-visual-comparison/server/scripts
+
+python tpEnpMatrixModule.py -c tianjin -t 0
+python tpEnpMatrixModule.py -c tianjin -t 1
+python tpEnpMatrixModule.py -c tianjin -t 2
+python tpEnpMatrixModule.py -c tianjin -t 3
+python tpEnpMatrixModule.py -c tianjin -t 4
+python tpEnpMatrixModule.py -c tianjin -t 5
+python tpEnpMatrixModule.py -c tianjin -t 6
