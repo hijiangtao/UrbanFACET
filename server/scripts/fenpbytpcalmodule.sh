f4657e35cc8bbbcab7c9a1fe789eb54c6eb50694
Joe Jiang <hijiangtao@gmail.com>
2017-03-03 16:11:58 +0800

    update: filtered entropy calculation shell script

--- server/scripts/fenpbytpcalmodule.sh
+++ server/scripts/fenpbytpcalmodule.sh
@@ -8,8 +8,8 @@ python fEnpByTPCalModule.py -c tangshan -t 3
 python fEnpByTPCalModule.py -c tangshan -t 4
 python fEnpByTPCalModule.py -c tangshan -t 5
 python fEnpByTPCalModule.py -c tangshan -t 6
-# python fEnpByTPCalModule.py -c tangshan -t 7
-# python fEnpByTPCalModule.py -c tangshan -t 8
+python fEnpByTPCalModule.py -c tangshan -t 7
+python fEnpByTPCalModule.py -c tangshan -t 8
 
 python fEnpByTPCalModule.py -c tianjin -t 0
 python fEnpByTPCalModule.py -c tianjin -t 1
@@ -18,8 +18,8 @@ python fEnpByTPCalModule.py -c tianjin -t 3
 python fEnpByTPCalModule.py -c tianjin -t 4
 python fEnpByTPCalModule.py -c tianjin -t 5
 python fEnpByTPCalModule.py -c tianjin -t 6
-# python fEnpByTPCalModule.py -c tianjin -t 7
-# python fEnpByTPCalModule.py -c tianjin -t 8
+python fEnpByTPCalModule.py -c tianjin -t 7
+python fEnpByTPCalModule.py -c tianjin -t 8
 
 python fEnpByTPCalModule.py -c zhangjiakou -t 0
 python fEnpByTPCalModule.py -c zhangjiakou -t 1
@@ -28,5 +28,5 @@ python fEnpByTPCalModule.py -c zhangjiakou -t 3
 python fEnpByTPCalModule.py -c zhangjiakou -t 4
 python fEnpByTPCalModule.py -c zhangjiakou -t 5
 python fEnpByTPCalModule.py -c zhangjiakou -t 6
-# python fEnpByTPCalModule.py -c zhangjiakou -t 7
-# python fEnpByTPCalModule.py -c zhangjiakou -t 8
+python fEnpByTPCalModule.py -c zhangjiakou -t 7
+python fEnpByTPCalModule.py -c zhangjiakou -t 8
