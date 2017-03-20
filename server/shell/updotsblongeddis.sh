13d511863b6bca3cc5515aad02a82cab74b8fa07
Joe Jiang <hijiangtao@gmail.com>
2017-03-20 11:44:32 +0800

    add: For-loop Exception

--- server/shell/updotsblongeddis.sh
+++ server/shell/updotsblongeddis.sh
@@ -1,7 +1,7 @@
 
 cd /home/taojiang/git/living-modes-visual-comparison/server/scripts
 
-# python ./upDotsBlongedDis.py -c beijing -d /enigma/tao.jiang/datasets/JingJinJi/records/disinfo
+python ./upDotsBlongedDis.py -c beijing -d /enigma/tao.jiang/datasets/JingJinJi/records/disinfo
 python ./upDotsBlongedDis.py -c tianjin -d /enigma/tao.jiang/datasets/JingJinJi/records/disinfo
 python ./upDotsBlongedDis.py -c zhangjiakou -d /enigma/tao.jiang/datasets/JingJinJi/records/disinfo
 python ./upDotsBlongedDis.py -c tangshan -d /enigma/tao.jiang/datasets/JingJinJi/records/disinfo
