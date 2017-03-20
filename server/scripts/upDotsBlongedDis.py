cdb144a6886c444f8ed2d29901e4a259762b98c3
Joe Jiang <hijiangtao@gmail.com>
2017-03-20 10:33:56 +0800

    add: upDotsBlongedDis script

--- server/scripts/upDotsBlongedDis.py
+++ server/scripts/upDotsBlongedDis.py
@@ -10,25 +10,39 @@ import sys
 import getopt
 import json
 from shapely.geometry import shape, Point
-from CommonFunc import getCityLocs, connectMongo, matrixtofile
+from CommonFunc import getCityLocs, connectMongo, matrixtofile, getAbbName, getDistrictsNum, getDisIndex
 
-def upDotsBlongedDis(city):
+def upDotsBlongedDis(city, dic):
 	# 获得 grids 
 	grids = [] # 存储结果
 	conn, db = connectMongo('tdnormal')
 	GRID = db['newgrids_%s' % city]
 
+	# 获取所有 grids 结果
 	grids = list( GRID.find({}, {"properties.uid": 1, "properties.center": 1}) )
+
+	# 构建栅栏数组
+	disobjs = []
+	with open(os.path.join(dic, getAbbName(city)+'.json')) as f:
+		data = json.load(f)
+		features = data.features
+
+		for each in features:
+			disobjs.append({
+				'name': each['properties']['name'],
+				'geo': shape(each['geometry'])
+			})
+		# 处理围栏数据
 
 	for each in grids:
 		coords = each.properties.center.coordinates
 		point = Point(coords[0], coords[1])
-		index = calDistrictIndex(point)
+		index = getDisIndex(point, disobjs)
+		if index != -1:
+			grids.append([each.properties.uid, index])
 
+	print "City %s owns valid distict grids %d" % (city, len(grids))
 	return grids
-
-def calDistrictIndex():
-	pass
 
 def main(argv):
 	# 初始化参数
@@ -38,17 +52,18 @@ def main(argv):
 		print str(err)
 		sys.exit(2)
 
-	city, dic = 'beijing', '/enigma/tao.jiang/datasets/JingJinJi/pois/beijing'
+	city, dic = 'beijing', '/home/tao.jiang/git/living-modes-visual-comparison/conf/data'
 	for opt, arg in opts:
 		if opt == '-h':
-			usage()
+			print 'No Doc Now.'
 			sys.exit()
 		elif opt in ("-c", "--city"):
 			city = arg
 		elif opt in ("-d", "--direcotry"):
 			dic = os.path.join(arg, city)
 
-	upDotsBlongedDis(city)
+	grids = upDotsBlongedDis(city, dic)
+	matrixtofile(grids, os.path.join(dic, 'grids'))
 
 if __name__ == '__main__':
 	main(sys.argv[1:])
